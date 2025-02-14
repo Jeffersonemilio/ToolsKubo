db5023af00678778ea7ee3d750449fa4459ded8f
User <user@iMac-de-User.local>
2025-02-14 10:17:44 -0300

    PIXELOK

--- src/lib/facebook-pixel.ts
+++ src/lib/facebook-pixel.ts
@@ -1,18 +1,29 @@
+type FacebookPixelFunction = (action: string, event: string, params?: object) => void;
+
+interface FacebookPixel extends FacebookPixelFunction {
+  callMethod?: (...args: any[]) => void;
+  queue?: any[];
+  loaded?: boolean;
+  version?: string;
+  push?: Function;
+}
+
 declare global {
   interface Window {
-    fbq: any;
+    fbq?: FacebookPixel;
+    _fbq?: FacebookPixel;
   }
 }
 
 export const FB_PIXEL_ID = '902536777459948'
 
 export const pageview = () => {
-  window.fbq('track', 'PageView')
+  window.fbq?.('track', 'PageView')
 }
 
 // https://developers.facebook.com/docs/facebook-pixel/advanced/
 export const lead = (name: string, phone: string) => {
-  window.fbq('track', 'Lead', {
+  window.fbq?.('track', 'Lead', {
     content_name: 'Quiz de Análise de Times Comerciais',
     content_category: 'Quiz',
     user_data: {
@@ -28,12 +39,14 @@ export const initFacebookPixel = () => {
     return
   }
 
-  if (window.fbq) return
+  if (typeof window.fbq === 'function') return
   
-  window.fbq = function() {
-    // @ts-ignore
-    window.fbq.callMethod ? window.fbq.callMethod.apply(window.fbq, arguments) : window.fbq.queue.push(arguments)
-  }
+  const pixelFunction = function(...args: [string, string, object?]) {
+    const fbq = window.fbq as FacebookPixel;
+    return fbq.callMethod ? fbq.callMethod(...args) : fbq.queue?.push(args);
+  } as FacebookPixel;
+  
+  window.fbq = pixelFunction;
   
   if (!window.fbq.queue) {
     window.fbq.queue = []
